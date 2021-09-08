d835f0c2c5b56dabee431f8a36b20e8293a73037
Jake Welde <jwelde@seas.upenn.edu>
2021-09-08 10:51:42 -0400

    iterate thru FK as well and world/rviz configs

--- labs/lab1/visualize.py
+++ labs/lab1/visualize.py
@@ -15,33 +15,9 @@ from PlanarIK import PlanarIK
 
 rospy.init_node("visualizer")
 
+# Using your solution code
 fk = FK()
 ik = PlanarIK()
-
-#################
-##  IK Targets ##
-#################
-
-# TODO: Try testing your own targets!
-
-targets = [
-    {
-        'o': np.array([0.1, 0.8]),
-        'theta': 0
-    },
-    {
-        'o': np.array([.4, .6]),
-        'theta': pi/4
-    },
-    {
-        'o': np.array([0.4, -0.4]),
-        'theta': pi/2+0.2
-     },
-    {
-        'o': np.array([.1, .3]),
-        'theta': 5 * pi/4
-    }
-]
 
 #########################
 ##  RViz Communication ##
@@ -90,24 +66,67 @@ def show_target(target):
     T0_target = tf.transformations.translation_matrix(np.array([x,0,z])) @ tf.transformations.euler_matrix(0,-theta-pi/2,pi)
     show_pose(T0_target,"target")
 
-#####################
-##  Test Execution ##
-#####################
+
+########################
+##  FK Configurations ##
+########################
+
+# TODO: Try testing other configurations!
+
+# The first configuration below matches the dimensional drawing in the handout
+configurations = [
+    np.array([ 0,    0,     0, -pi/2,     0, pi/2, pi/4 ]),
+    np.array([ pi/2, 0,  pi/4, -pi/2, -pi/2, pi/2,    0 ]),
+    np.array([ 0,    0, -pi/2, -pi/4,  pi/2, pi,   pi/4 ]),
+]
+
+#################
+##  IK Targets ##
+#################
+
+# TODO: Try testing your own targets!
+
+targets = [
+    {
+        'o': np.array([0.1, 0.8]),
+        'theta': 0
+    },
+    {
+        'o': np.array([.4, .6]),
+        'theta': pi/4
+    },
+    {
+        'o': np.array([0.4, -0.4]),
+        'theta': pi/2+0.2
+     },
+    {
+        'o': np.array([.1, .3]),
+        'theta': 5 * pi/4
+    }
+]
+
+####################
+## Test Execution ##
+####################
 
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
-        print("usage:\n\tpython visualize.py FK\npython visualize.py IK")
+        print("usage:\n\tpython visualize.py FK\n\tpython visualize.py IK")
         exit()
 
     arm = ArmController(on_state_callback=show_all_FK)
 
     if sys.argv[1] == 'FK':
 
-        # TODO: try different configurations!
-        q = np.array([0, 0, 0, -pi/2, 0, pi/2, pi/4])
+        # Iterates through the given configurations, visualizing your FK solution
+        # Try editing the configurations list above to do more testing!
+        for i, q in enumerate(configurations):
+            print("Moving to configuration " + str(i) + "...")
+            arm.move_to_position(q)
+            if i < len(configurations) - 1:
+                input("Press Enter to move to next configuration...")
 
-        print(q)
         arm.move_to_position(q)
 
     elif sys.argv[1] == 'IK':
@@ -118,10 +137,10 @@ if __name__ == "__main__":
             print("Moving to target " + str(i) + "...")
             show_target(target)
             solutions = ik.panda_ik(target)
-            q = solutions[0,:]
+            q = solutions[0,:] # choose the first of multiple solutions
             arm.move_to_position(q)
             if i < len(targets) - 1:
                 input("Press Enter to move to next target...")
+
     else:
         print("invalid option")
-        exit()
