20941885dd4f2e81a51e53b742da6db7fb04bc83
Jake Welde <jwelde@seas.upenn.edu>
2021-09-28 21:37:45 -0400

    updating stub code

--- lib/IK_velocity.py
+++ lib/IK_velocity.py
@@ -2,18 +2,21 @@ import numpy as np
 from lib.calcJacobian import calcJacobian
 
 
-def IK_velocity(q_in, v_in, omega_in, joint):
+def IK_velocity(q_in, v_in, omega_in):
     """
     :param q: 0 x 7 vector corresponding to the robot's current configuration.
-    :param v: The desired linear velocity in the world frame. If any element is Nan, then that velocity can be anything
-    :param omega: The desired angular velocity in the world frame. If any element is Nan, then that velocity can be
-                  anything
-    :param joint: an integer in [0,7] corresponding to which joint you are tracking
+    :param v: The desired linear velocity in the world frame. If any element is
+    Nan, then that velocity can be anything
+    :param omega: The desired angular velocity in the world frame. If any
+    element is Nan, then that velocity is unconstrained i.e. it can be anything
     :return:
     dq - 0 x 7 vector corresponding to the joint velocities. If v and omega
-         are infeasible, then dq should minimize the least squares error. If v and omega have multiple solutions then
-         you should minimize the l2 norm of dq
+         are infeasible, then dq should minimize the least squares error. If v
+         and omega have multiple solutions, then you should select the solution
+         that minimizes the l2 norm of dq
     """
+
+    ## STUDENT CODE GOES HERE
 
     dq = np.zeros(7)
 
