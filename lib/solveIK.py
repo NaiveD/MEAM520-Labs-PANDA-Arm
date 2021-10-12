d4fc53a6bb37ed950ff18c5dec7d3466f98e23fb
Jake Welde <jwelde@seas.upenn.edu>
2021-10-12 10:49:21 -0400

    stub code initial commit nothing but centering task and inverse scaffolding implemented

--- lib/solveIK.py
+++ lib/solveIK.py
@@ -1,63 +1,264 @@
 import numpy as np
-from math import pi
+from math import pi, acos
+from scipy.linalg import null_space
 
 from lib.calcJacobian import calcJacobian
 from lib.calculateFK import FK
 from lib.IK_velocity import IK_velocity
 
-class IK():
+class IK:
 
-    def __init__(self):
-        fk = FK()
+    # JOINT LIMITS
+    lower = np.array([-2.8973,-1.7628,-2.8973,-3.0718,-2.8973,-0.0175,-2.8973])
+    upper = np.array([2.8973,1.7628,2.8973,-0.0698,2.8973,3.7525,2.8973])
 
-    def inverse(self, target, seed, linear_tol=1e-6, angular_tol=1e-3):
+    def __init__(self,linear_tol=1e-4, angular_tol=1e-3, max_steps=500, min_step_size=1e-5):
         """
-        INPUT:
+        Constructs an optimization-based IK solver with given solver parameters.
+        Default parameters are tuned to reasonable values.
+
+        PARAMETERS:
+        linear_tol - the maximum distance in meters between the target end
+        effector origin and actual end effector origin for a solution to be
+        considered successful
+        angular_tol - the maximum angle of rotation in radians between the target
+        end effector frame and actual end effector frame for a solution to be
+        considered successful
+        max_steps - number of iterations before the algorithm must terminate
+        min_step_size - the minimum step size before concluding that the
+        optimizer has converged
+        """
+
+        # THIS FUNCTION HAS BEEN FULLY IMPLEMENTED FOR YOU
+
+        # solver parameters
+        self.linear_tol = linear_tol
+        self.angular_tol = angular_tol
+        self.max_steps = max_steps
+        self.min_step_size = min_step_size
+
+        self.fk = FK()
+        # compute middle of range of motion of each joint
+        self.center = self.lower + (self.upper - self.lower) / 2
+
+    ####################
+    ## Task Functions ##
+    ####################
+
+    def end_effector_task(self,q,target):
+        """
+        Primary task for IK solver. Computes a joint velocity which will reduce
+        the error between the target end effector pose and the current end
+        effector pose (corresponding to configuration q).
+
+        INPUTS:
+        q - the current joint configuration, a "best guess" so far for the final answer
+        target - a 4x4 numpy array containing the desired end effector pose
+
+        OUTPUTS:
+        dq - a desired joint velocity to perform this task, which will smoothly
+        decay to zero magnitude as the task is achieved
+        """
+
+        ## STUDENT CODE STARTS HERE
+
+        dq = np.zeros(7)
+
+        ## END STUDENT CODE
+
+        return dq
+
+    def joint_centering_task(self,q,rate=5e-1):
+        """
+        Secondary task for IK solver. Computes a joint velocity which will
+        reduce the offset between each joint's angle and the center of its range
+        of motion. This secondary task acts as a "soft constraint" which
+        encourages the solver to choose solutions within the allowed range of
+        motion for the joints.
+
+        INPUTS:
+        q - the joint angles
+        rate - a tunable parameter dictating how quickly to try to center the
+        joints. Reducing this parameter improves convergence stability for the
+        primary task, but also requires more solver iterations.
+
+        OUTPUTS:
+        dq - a desired joint velocity to perform this task, which will smoothly
+        decay to zero magnitude as the task is achieved
+        """
+
+        # THIS FUNCTION HAS BEEN FULLY IMPLEMENTED FOR YOU
+
+        # normalize the offsets of all joints to range from -1 to 1 within the allowed range
+        offset = 2 * (q - self.center) / (self.upper - self.lower)
+        dq = rate * -offset # proportional term (implied quadratic cost)
+
+        return dq
+
+    ######################
+    ## Helper Functions ##
+    ######################
+
+    @staticmethod
+    def direction_to_move(target, current):
+        """
+        Helper function for the End Effector Task. Computes an end effector
+        velocity which will bring the end effector closer to the target position
+        and orientation.
+
+        INPUTS:
+        target - 4x4 numpy array representing the desired transformation from
+        end effector to world
+        target - 4x4 numpy array representing the "current" end effector orientation
+
+        OUTPUTS:
+        v - a 1x3 numpy array containing the linear velocity with which to move
+        the end effector to decrease the position error, expressed in the world frame
+        omega - a 1x3 numpy array containing the angular velocity with which to
+        move the end effector to decrease the orientation error, expressed in the world frame
+        """
+
+        ## STUDENT CODE STARTS HERE
+
+        v = np.zeros(3)
+        omega = np.zeros(3)
+
+        ## END STUDENT CODE
+
+        return v, omega
+
+    @staticmethod
+    def distance_and_angle(G, H):
+        """
+        Helper function which computes the distance and angle between any two
+        transforms. Can be used to decide whether two transforms can be
+        considered the same to a certain linear and angular tolerance.
+
+        INPUTS:
+        G - a 4x4 numpy array representing some homogenous transformation
+        H - a 4x4 numpy array representing some homogenous transformation
+
+        OUTPUTS:
+        distance - the distance in meters between the origins of G & H
+        angle - the magnitude in radians of the angle between the orientations
+        of G & H
+
+
+        """
+
+        ## STUDENT CODE STARTS HERE
+
+        distance = 0
+        angle = 0
+
+        ## END STUDENT CODE
+
+        return distance, angle
+
+    def is_valid_solution(self,q,target):
+        """
+        Given a candidate solution, determine if it achieves the primary task
+        and also respects the joint limits.
+
+        INPUTS
+        q - the candidate solution, namely the joint angles
+        target - 4x4 numpy array representing the desired transformation from
+        end effector to world
+
+        OUTPUTS:
+        success - a Boolean which is True if and only if the candidate solution
+        produces an end effector pose which is within the given linear and
+        angular tolerances of the target pose, and also respects the joint
+        limits.
+        """
+
+        ## STUDENT CODE STARTS HERE
+
+        success = False
+
+        ## END STUDENT CODE
+
+        return success
+
+    ###############################
+    ## Inverse Kinematics Solver ##
+    ###############################
+
+    def inverse(self, target, seed):
+        """
+        Uses gradient descent to solve the full inverse kinematics of the Panda robot.
+        Terminates according to step size and max iterations passed to constructor.
+
+        INPUTS:
         target - 4x4 numpy array representing the desired transformation from
         end effector to world
         seed - 1x7 vector of joint angles [q0, q1, q2, q3, q4, q5, q6], which
         is the "initial guess" from which to proceed with optimization
-        linear_tol - the maximum distance between the target end effector origin
-        and actual end effector origin for a solution to be considered successful
-        linear_tol - the maximum angle of rotation between the target end
-        effector frame and actual end effector frame for a solution to be
-        considered successful
 
         OUTPUTS:
         q - 1x7 vector of joint angles [q0, q1, q2, q3, q4, q5, q6], giving the
         solution if success is True or the closest guess if success is False.
         success - True if the IK algorithm successfully found a configuration
         which achieves the target within the given tolerance. Otherwise False
+        rollout - a list containing the guess for q at each iteration of the algorithm
         """
 
-        # Your code starts here
+        q = seed
+        rollout = []
 
-        q = np.array([0,0,0,-pi/2,0,pi/2,pi/4])
-        success = False
+        while True:
 
-        # Your code ends here
+            rollout.append(q)
 
-        return q, success
+            # Primary Task - Achieve End Effector Pose
+            dq_ik = self.end_effector_task(q,target)
 
-    # feel free to define additional helper methods to modularize your solution
+            # Secondary Task - Center Joints
+            dq_center = self.joint_centering_task(q)
+
+            ## STUDENT CODE STARTS HERE
+
+            # Task Prioritization
+            dq = np.zeros(7) # TODO: implement me!
+
+            # Termination Conditions
+            if True: # TODO: check termination conditions
+                break # exit the while loop if conditions are met!
+
+            ## END STUDENT CODE
+
+            q = q + dq
+
+        success = self.is_valid_solution(q,target)
+        return q, success, rollout
+
+
+################################
+## Simple Testing Environment ##
+################################
 
 if __name__ == "__main__":
 
-    ik = IK()
+    ik = IK() # uses default solver parameters
 
     # matches figure in the handout
     seed = np.array([0,0,0,-pi/2,0,pi/2,pi/4])
 
     target = np.array([
-        [1,0,0,.5],
-        [0,1,0,.5],
-        [0,0,1,.5],
+        [1,0,0,0.6],
+        [0,-1,0,0],
+        [0,0,-1,0],
         [0,0,0, 1],
     ])
 
-    q, success = ik.inverse(target, seed)
+    q, success, rollout = ik.inverse(target, seed)
 
-    if success:
-        print("Solution found,\nq=",q)
-    else:
-        print("Solution not found, best guess is\nq =",q)
+    np.set_printoptions(suppress=True,precision=5)
+    for i, q in enumerate(rollout):
+        joints, pose = ik.fk.forward(q)
+        d, ang = IK.distance_and_angle(target,pose)
+        print('iteration:',i,' q =',q, ' d={d:3.4f}  ang={ang:3.3f}'.format(d=d,ang=ang))
+
+    print("Success: ",success)
+    print("Solution: ",q)
+    print("Iterations:", len(rollout))
