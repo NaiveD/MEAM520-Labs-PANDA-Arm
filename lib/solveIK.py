ef49147c7be00075620d5fec68af1def0e07effc
Jake Welde <jwelde@seas.upenn.edu>
2021-10-12 14:02:48 -0400

    Propogate updates to stub code from solution interface changes, etc.

--- lib/solveIK.py
+++ lib/solveIK.py
@@ -12,6 +12,9 @@ class IK:
     lower = np.array([-2.8973,-1.7628,-2.8973,-3.0718,-2.8973,-0.0175,-2.8973])
     upper = np.array([2.8973,1.7628,2.8973,-0.0698,2.8973,3.7525,2.8973])
 
+    center = lower + (upper - lower) / 2 # compute middle of range of motion of each joint
+    fk = FK()
+
     def __init__(self,linear_tol=1e-4, angular_tol=1e-3, max_steps=500, min_step_size=1e-5):
         """
         Constructs an optimization-based IK solver with given solver parameters.
@@ -37,102 +40,53 @@ class IK:
         self.max_steps = max_steps
         self.min_step_size = min_step_size
 
-        self.fk = FK()
-        # compute middle of range of motion of each joint
-        self.center = self.lower + (self.upper - self.lower) / 2
-
-    ####################
-    ## Task Functions ##
-    ####################
-
-    def end_effector_task(self,q,target):
-        """
-        Primary task for IK solver. Computes a joint velocity which will reduce
-        the error between the target end effector pose and the current end
-        effector pose (corresponding to configuration q).
-
-        INPUTS:
-        q - the current joint configuration, a "best guess" so far for the final answer
-        target - a 4x4 numpy array containing the desired end effector pose
-
-        OUTPUTS:
-        dq - a desired joint velocity to perform this task, which will smoothly
-        decay to zero magnitude as the task is achieved
-        """
-
-        ## STUDENT CODE STARTS HERE
-
-        dq = np.zeros(7)
-
-        ## END STUDENT CODE
-
-        return dq
-
-    def joint_centering_task(self,q,rate=5e-1):
-        """
-        Secondary task for IK solver. Computes a joint velocity which will
-        reduce the offset between each joint's angle and the center of its range
-        of motion. This secondary task acts as a "soft constraint" which
-        encourages the solver to choose solutions within the allowed range of
-        motion for the joints.
-
-        INPUTS:
-        q - the joint angles
-        rate - a tunable parameter dictating how quickly to try to center the
-        joints. Reducing this parameter improves convergence stability for the
-        primary task, but also requires more solver iterations.
-
-        OUTPUTS:
-        dq - a desired joint velocity to perform this task, which will smoothly
-        decay to zero magnitude as the task is achieved
-        """
-
-        # THIS FUNCTION HAS BEEN FULLY IMPLEMENTED FOR YOU
-
-        # normalize the offsets of all joints to range from -1 to 1 within the allowed range
-        offset = 2 * (q - self.center) / (self.upper - self.lower)
-        dq = rate * -offset # proportional term (implied quadratic cost)
-
-        return dq
 
     ######################
     ## Helper Functions ##
     ######################
 
     @staticmethod
-    def direction_to_move(target, current):
+    def displacement_and_axis(target, current):
         """
-        Helper function for the End Effector Task. Computes an end effector
-        velocity which will bring the end effector closer to the target position
-        and orientation.
+        Helper function for the End Effector Task. Computes the displacement
+        vector and axis of rotation from the current frame to the target frame
+
+        This data can also be interpreted as an end effector velocity which will
+        bring the end effector closer to the target position and orientation.
 
         INPUTS:
         target - 4x4 numpy array representing the desired transformation from
         end effector to world
-        target - 4x4 numpy array representing the "current" end effector orientation
+        current - 4x4 numpy array representing the "current" end effector orientation
 
         OUTPUTS:
-        v - a 1x3 numpy array containing the linear velocity with which to move
-        the end effector to decrease the position error, expressed in the world frame
-        omega - a 1x3 numpy array containing the angular velocity with which to
-        move the end effector to decrease the orientation error, expressed in the world frame
+        displacement - a 3-element numpy array containing the displacement from
+        the current frame to the target frame, expressed in the world frame
+        axis - a 3-element numpy array containing the axis of the rotation from
+        the current frame to the end effector frame. The magnitude of this vector
+        must be sin(angle), where angle is the angle of rotation around this axis
         """
 
         ## STUDENT CODE STARTS HERE
 
-        v = np.zeros(3)
-        omega = np.zeros(3)
+        displacement = np.zeros(3)
+        axis = np.zeros(3)
 
         ## END STUDENT CODE
 
-        return v, omega
+        return displacement, axis
 
     @staticmethod
     def distance_and_angle(G, H):
         """
         Helper function which computes the distance and angle between any two
-        transforms. Can be used to decide whether two transforms can be
-        considered the same to a certain linear and angular tolerance.
+        transforms.
+
+        This data can be used to decide whether two transforms can be
+        considered equal within a certain linear and angular tolerance.
+
+        Be careful! Using the axis output of displacement_and_axis to compute
+        the angle will result in incorrect results when |angle| > pi/2
 
         INPUTS:
         G - a 4x4 numpy array representing some homogenous transformation
@@ -140,20 +94,19 @@ class IK:
 
         OUTPUTS:
         distance - the distance in meters between the origins of G & H
-        angle - the magnitude in radians of the angle between the orientations
-        of G & H
+        angle - the angle in radians between the orientations of G & H
 
 
         """
 
-        ## STUDENT CODE STARTS HERE
+       ## STUDENT CODE STARTS HERE
 
-        distance = 0
-        angle = 0
+       distance = 0
+       angle = 0
 
-        ## END STUDENT CODE
+       ## END STUDENT CODE
 
-        return distance, angle
+       return distance, angle
 
     def is_valid_solution(self,q,target):
         """
@@ -180,6 +133,62 @@ class IK:
 
         return success
 
+    ####################
+    ## Task Functions ##
+    ####################
+
+    @staticmethod
+    def end_effector_task(q,target):
+        """
+        Primary task for IK solver. Computes a joint velocity which will reduce
+        the error between the target end effector pose and the current end
+        effector pose (corresponding to configuration q).
+
+        INPUTS:
+        q - the current joint configuration, a "best guess" so far for the final answer
+        target - a 4x4 numpy array containing the desired end effector pose
+
+        OUTPUTS:
+        dq - a desired joint velocity to perform this task, which will smoothly
+        decay to zero magnitude as the task is achieved
+        """
+
+        ## STUDENT CODE STARTS HERE
+
+        dq = np.zeros(7)
+
+        ## END STUDENT CODE
+
+        return dq
+
+    @staticmethod
+    def joint_centering_task(q,rate=5e-1):
+        """
+        Secondary task for IK solver. Computes a joint velocity which will
+        reduce the offset between each joint's angle and the center of its range
+        of motion. This secondary task acts as a "soft constraint" which
+        encourages the solver to choose solutions within the allowed range of
+        motion for the joints.
+
+        INPUTS:
+        q - the joint angles
+        rate - a tunable parameter dictating how quickly to try to center the
+        joints. Turning this parameter improves convergence behavior for the
+        primary task, but also requires more solver iterations.
+
+        OUTPUTS:
+        dq - a desired joint velocity to perform this task, which will smoothly
+        decay to zero magnitude as the task is achieved
+        """
+
+        # THIS FUNCTION HAS BEEN FULLY IMPLEMENTED FOR YOU
+
+        # normalize the offsets of all joints to range from -1 to 1 within the allowed range
+        offset = 2 * (q - IK.center) / (IK.upper - IK.lower)
+        dq = rate * -offset # proportional term (implied quadratic cost)
+
+        return dq
+
     ###############################
     ## Inverse Kinematics Solver ##
     ###############################
@@ -187,7 +196,6 @@ class IK:
     def inverse(self, target, seed):
         """
         Uses gradient descent to solve the full inverse kinematics of the Panda robot.
-        Terminates according to step size and max iterations passed to constructor.
 
         INPUTS:
         target - 4x4 numpy array representing the desired transformation from
@@ -232,28 +240,28 @@ class IK:
         success = self.is_valid_solution(q,target)
         return q, success, rollout
 
-
 ################################
 ## Simple Testing Environment ##
 ################################
 
 if __name__ == "__main__":
 
-    ik = IK() # uses default solver parameters
+    np.set_printoptions(suppress=True,precision=5)
+
+    ik = IK()
 
     # matches figure in the handout
     seed = np.array([0,0,0,-pi/2,0,pi/2,pi/4])
 
     target = np.array([
-        [1,0,0,0.6],
-        [0,-1,0,0],
-        [0,0,-1,0],
+        [0,-1,0,0.3],
+        [-1,0,0,0],
+        [0,0,-1,.5],
         [0,0,0, 1],
     ])
 
     q, success, rollout = ik.inverse(target, seed)
 
-    np.set_printoptions(suppress=True,precision=5)
     for i, q in enumerate(rollout):
         joints, pose = ik.fk.forward(q)
         d, ang = IK.distance_and_angle(target,pose)
